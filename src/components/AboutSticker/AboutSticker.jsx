320f90f6af9c28cb345ccba71c8f57394ab881f0
shm <shm.almeda@berkeley.edu>
2026-09-12 12:42:43 -0700

    update about sticker

--- src/components/AboutSticker/AboutSticker.jsx
+++ src/components/AboutSticker/AboutSticker.jsx
@@ -4,8 +4,7 @@ import "./AboutSticker.css";
 export default function AboutSticker({ onClick }) {
   return (
     <StickerButton className="about-sticker" title="about" onClick={onClick}>
-      <img src="/assets/question-mark.png" alt="About" />
-      about
+      <img style={{ width: "24px", height: "auto" }} src="/assets/questionmark.png" alt="About" />
     </StickerButton>
   );
 }
